f0dbcc70d436af99707989664c40d4ab079b0f39
SupremeCookie <davidlskatsma@gmail.com>
2021-12-27 13:31:46 +0100

    Starting a general debug input class, have some of the general actions defined already

---
+++ Input/GeneralActions.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomInput
+{
+	public enum GeneralActions
+	{
+		Up,
+		Down,
+		Left,
+		Right,
+
+		RotateLeft,
+		RotateRight,
+
+
+		Count,  // Note DK: Always keep last
+	}
+
+	public enum MouseActions
+	{
+		Click_Left,
+		Click_Right,
+		Click_Middle,
+
+		Count,
+	}
+}
