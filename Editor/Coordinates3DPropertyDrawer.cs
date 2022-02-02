f5fc90a16aada34ac9baa8383297c88985788e0c
SupremeCookie <davidlskatsma@gmail.com>
2022-02-02 09:26:23 +0100

    Added coordinates property drawers

---
+++ Editor/Coordinates3DPropertyDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Coordinates3D))]
+public class Coordinates3DPropertyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.BeginProperty(position, label, property);
+
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+		float width = 100;
+		var xRect = new Rect(position.x, position.y, width, position.height);
+		var yRect = new Rect(position.x + width, position.y, width, position.height);
+		var zRect = new Rect(position.x + (width * 2), position.y, width, position.height);
+
+		EditorGUI.PropertyField(xRect, property.FindPropertyRelative("x"), GUIContent.none);
+		EditorGUI.PropertyField(yRect, property.FindPropertyRelative("y"), GUIContent.none);
+		EditorGUI.PropertyField(zRect, property.FindPropertyRelative("z"), GUIContent.none);
+
+		EditorGUI.EndProperty();
+	}
+}
---
+++ Editor/CoordinatesPropertyDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Coordinates))]
+public class CoordinatesPropertyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.BeginProperty(position, label, property);
+
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+		float width = 100;
+		var xRect = new Rect(position.x, position.y, width, position.height);
+		var yRect = new Rect(position.x + width, position.y, width, position.height);
+
+		EditorGUI.PropertyField(xRect, property.FindPropertyRelative("x"), GUIContent.none);
+		EditorGUI.PropertyField(yRect, property.FindPropertyRelative("y"), GUIContent.none);
+
+		EditorGUI.EndProperty();
+	}
+}
