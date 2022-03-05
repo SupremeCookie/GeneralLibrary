948166ed42c211492ac6023d594e6f5a763545eb
SupremeCookie <davidlskatsma@gmail.com>
2022-03-05 19:45:34 +0100

    Added a test class to see how ui elements handle under differing resolutions

---
+++ Tests/TestUIResolutionInfluence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TestUIResolutionInfluence : MonoBehaviour
+{
+	private RectTransform rect;
+	private Canvas canvas;
+
+	[SerializeField] [Readonly] private Vector2 sizeDelta;
+	[SerializeField] [Readonly] private Vector2 sizeDeltaScaled;
+	[SerializeField] [Readonly] private Vector2 position;
+	[SerializeField] [Readonly] private Vector2 positionScaled;
+
+	[SerializeField] [Readonly] private MinMax bbox;
+	[SerializeField] [Readonly] private MinMax bboxScaled;
+
+
+	private void Awake()
+	{
+		rect = this.GetComponent<RectTransform>();
+		canvas = GetComponentInParent<Canvas>();
+	}
+
+	private void Update()
+	{
+		var canvasScalar = canvas.scaleFactor;
+
+		sizeDelta = rect.sizeDelta;
+		sizeDeltaScaled = rect.sizeDelta * canvasScalar;
+		position = rect.position;
+		positionScaled = rect.position * canvasScalar;
+		bbox = new MinMax(position + (sizeDelta * -0.5f), position + (sizeDelta * 0.5f));
+		bboxScaled = new MinMax(position + (sizeDelta * -0.5f) * canvasScalar, position + (sizeDelta * 0.5f) * canvasScalar);
+	}
+}
