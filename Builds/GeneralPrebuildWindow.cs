1b10023a44c0e9d23fed340ae3922b5b4ad6b2bf
SupremeCookie <davidlskatsma@gmail.com>
2022-05-18 23:22:43 +0200

    Made some build related usability code

---
+++ Builds/GeneralPrebuildWindow.cs
@@ -0,0 +1,57 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+
+public class BuildWindowHandler
+{
+	[InitializeOnLoadMethod]
+	public static void OnLoadingRuntime()
+	{
+		BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayer);
+	}
+
+	public static void BuildPlayer(BuildPlayerOptions options)
+	{
+		var prebuildWindow = (GeneralPreBuildWindow)EditorWindow.GetWindow(typeof(GeneralPreBuildWindow));
+		GeneralPreBuildWindow.buildOptions = options;
+		prebuildWindow.Show();
+	}
+}
+
+public class GeneralPreBuildWindow : BuildPlayerWindow
+{
+	private const string DEBUG_MENU = "DEBUG_MENU";
+
+	public static BuildPlayerOptions buildOptions { get { return IniControl.GlobalData.BuildOptions; } set { IniControl.GlobalData.BuildOptions = value; } }
+
+	private void OnGUI()
+	{
+		var scriptingDefinesForTarget = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildOptions.targetGroup);
+
+		bool debugMenuIsEnabled = scriptingDefinesForTarget.Contains(DEBUG_MENU);
+		var newDebugMenuIsEnabled = GUILayout.Toggle(debugMenuIsEnabled, "Is Debug Menu Enabled");
+
+		if (debugMenuIsEnabled != newDebugMenuIsEnabled)
+		{
+			if (!newDebugMenuIsEnabled)
+			{
+				var startIndexOfString = scriptingDefinesForTarget.IndexOf(DEBUG_MENU);
+				scriptingDefinesForTarget = scriptingDefinesForTarget.Remove(startIndexOfString, DEBUG_MENU.Length);
+			}
+			else
+			{
+				scriptingDefinesForTarget += $";{DEBUG_MENU}";
+			}
+
+			PlayerSettings.SetScriptingDefineSymbolsForGroup(buildOptions.targetGroup, scriptingDefinesForTarget);
+		}
+
+
+		if (GUILayout.Button("Build Player"))
+		{
+			BuildPipeline.BuildPlayer(buildOptions);
+		}
+	}
+}
+#endif
---
+++ Builds/SerializableBuildPlayerOptions.cs
@@ -0,0 +1,41 @@
+﻿
+#if UNITY_EDITOR
+using UnityEditor;
+
+[System.Serializable]
+public class SerializableBuildPlayerOptions
+{
+	public string[] scenes;
+	public string locationPathName;
+	public string assetBundleManifestPath;
+	public BuildTargetGroup targetGroup;
+	public BuildTarget target;
+	public BuildOptions options;
+
+	public static implicit operator BuildPlayerOptions(SerializableBuildPlayerOptions input)
+	{
+		return new BuildPlayerOptions
+		{
+			assetBundleManifestPath = input.assetBundleManifestPath,
+			locationPathName = input.locationPathName,
+			options = input.options,
+			scenes = input.scenes,
+			target = input.target,
+			targetGroup = input.targetGroup,
+		};
+	}
+
+	public static implicit operator SerializableBuildPlayerOptions(BuildPlayerOptions input)
+	{
+		return new SerializableBuildPlayerOptions
+		{
+			assetBundleManifestPath = input.assetBundleManifestPath,
+			locationPathName = input.locationPathName,
+			options = input.options,
+			scenes = input.scenes,
+			target = input.target,
+			targetGroup = input.targetGroup,
+		};
+	}
+}
+#endif
