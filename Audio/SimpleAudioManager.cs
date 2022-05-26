8cb023a15be90b07b8318252a8863924d9e8dd61
SupremeCookie <davidlskatsma@gmail.com>
2022-05-26 18:10:47 +0200

    Added a simplistic audio manager that can run a bgm track

---
+++ Audio/SimpleAudioManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class SimpleAudioManager : MonoBehaviour, IInitialisable, IUpdatable
+{
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private AudioClip[] audioClips;
+
+	private int currentAudio;
+
+	public bool IsInitialised { get; set; }
+
+	private void Start()
+	{
+		Init();
+	}
+
+	public void Init()
+	{
+		currentAudio = 0;
+
+		StartAudio(currentAudio);
+
+#if DEBUG_MENU
+		DrawGUI.Instance.OnDrawGUI += OnDrawGUI;
+#endif
+
+		IsInitialised = true;
+	}
+
+	public void UpdateComponent(float deltaSeconds)
+	{
+		if (!IsInitialised)
+		{
+			return;
+		}
+
+		if (!audioSource.isPlaying)
+		{
+			currentAudio++;
+			currentAudio %= audioClips.Length;
+
+			StartAudio(currentAudio);
+		}
+	}
+
+	private void StartAudio(int clip)
+	{
+		audioSource.clip = audioClips[clip];
+		audioSource.Play();
+	}
+
+#if DEBUG_MENU
+	private void OnDrawGUI()
+	{
+		if (DrawGUI.CreateGroup("Audio", out var group))
+		{
+			group.DrawLabel($"Current audio clip: ({currentAudio})");
+
+			float currentTime = audioSource.time;
+			group.DrawLabel($"Current time: ({currentTime})");
+		}
+	}
+#endif
+}
