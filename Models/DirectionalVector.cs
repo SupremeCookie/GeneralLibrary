85baac06f516e578e70cefc977b1d03dc05162c6
SupremeCookie <davidlskatsma@gmail.com>
2022-12-21 08:32:47 +0100

    DirectionalVector has a Cardinal variant added

--- Models/DirectionalVector.cs
+++ Models/DirectionalVector.cs
@@ -7,3 +7,10 @@ public struct DirectionalVector
 	public Vector2 Vector;
 	public OCTA_DIRECTION Direction;
 }
+
+[Serializable]
+public struct CardinalDirectionalVector
+{
+	public Vector2 Vector;
+	public CARDINAL_DIRECTION Direction;
+}
