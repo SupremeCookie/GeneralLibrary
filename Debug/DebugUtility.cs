27ed47344ca43df1d4688ac3c9bc45afa342bd6f
SupremeCookie <davidlskatsma@gmail.com>
2022-12-21 08:32:02 +0100

    Added a DebugUtility class which handles setting and retrieving colors for debugging purposes

---
+++ Debug/DebugUtility.cs
@@ -0,0 +1,16 @@
+﻿public static class DebugUtility
+{
+	private static UnityEngine.Color[] debugColors = new UnityEngine.Color[]
+	{
+			new UnityEngine.Color(1.0f, 1.0f, 0.0f, 1.0f),
+			new UnityEngine.Color(0.4f, 1.0f, 0.2f, 1.0f),
+			new UnityEngine.Color(0.0f, 1.0f, 1.0f, 1.0f),
+			new UnityEngine.Color(0.6f, 0.4f, 1.0f, 1.0f),
+			new UnityEngine.Color(1.0f, 0.0f, 0.4f, 1.0f),
+	};
+
+	public static UnityEngine.Color GetDebugColor(int index)
+	{
+		return debugColors[index % debugColors.Length];
+	}
+}
