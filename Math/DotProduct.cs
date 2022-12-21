36369aa241d8561f94f248f378249ae944494884
SupremeCookie <davidlskatsma@gmail.com>
2022-12-21 08:32:35 +0100

    Improved DotProduct by putting it into the global namespace

--- Math/DotProduct.cs
+++ Math/DotProduct.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 
-namespace RogueLike
+public static class DotProduct
 {
-	public static class DotProduct
+	public static float DotProductNormalized(Vector2 input, Vector2 input2)
 	{
-		public static float DotProductNormalized(Vector2 input, Vector2 input2)
-		{
-			input = input.normalized;
-			input2 = input2.normalized;
+		input = input.normalized;
+		input2 = input2.normalized;
 
-			return CalculateDotProduct(input, input2);
-		}
+		return CalculateDotProduct(input, input2);
+	}
 
-		public static float CalculateDotProduct(Vector2 input, Vector2 input2)
-		{
-			return (input.x * input2.x) + (input.y * input2.y);
-		}
+	public static float CalculateDotProduct(Vector2 input, Vector2 input2)
+	{
+		return (input.x * input2.x) + (input.y * input2.y);
 	}
 }
