e8431d1a67d5f862e4d3bbe6ba9d84d968952f6c
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2023-01-25 14:45:51 +0100

    Added a different point evaluationmode for when we use the rotary cw/ccw checker
    Added a new way to check cw/ccw by use of the Shoelace Formula

--- Triangulator/TriangleUtility.cs
+++ Triangulator/TriangleUtility.cs
@@ -1,5 +1,6 @@
 ﻿//#define USE_RANDOM_TRIANGLE_METHOD
-#define USE_ROTARY_WALKING_METHOD
+//#define USE_ROTARY_WALKING_METHOD
+#define USE_PROPER_MATHS
 
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,17 @@ namespace Triangulator
 {
 	public enum CWType { CW, CCW, }    // Clock-Wise, Counter-Clock-Wise
 
+#if USE_ROTARY_WALKING_METHOD
+	public enum RotaryEvaluationPointMode { Baricenter, RandomVertex, };
+#endif
+
 	public static class TriangleUtility
 	{
+#if USE_ROTARY_WALKING_METHOD
+		public static CWType CheckClockwiseTypeOfLoop(in List<Vector2> meshVertexLoop, RotaryEvaluationPointMode evalMode)
+#else
 		public static CWType CheckClockwiseTypeOfLoop(in List<Vector2> meshVertexLoop)
+#endif
 		{
 #if USE_RANDOM_TRIANGLE_METHOD
 			Vector2[] randomTriangle = new Vector2[]
@@ -23,12 +32,21 @@ namespace Triangulator
 			return GetCWTypeForTriangle(randomTriangle);
 #endif
 
-#if USE_ROTARY_WALKING_METHOD
+#if USE_ROTARY_WALKING_METHOD || USE_PROPER_MATHS
+#if USE_PROPER_MATHS
 			return GetCWTypeForVertexLoop(in meshVertexLoop);
+#else
+			return GetCWTypeForVertexLoop(in meshVertexLoop, evalMode);
 #endif
+#endif
+
 		}
 
+#if USE_ROTARY_WALKING_METHOD
+		public static CWType CheckClockwiseTypeOfLoop(in List<Coordinates> meshVertexLoop, RotaryEvaluationPointMode evalMode)
+#else
 		public static CWType CheckClockwiseTypeOfLoop(in List<Coordinates> meshVertexLoop)
+#endif
 		{
 #if USE_RANDOM_TRIANGLE_METHOD
 			Vector2[] randomTriangle = new Vector2[]
@@ -41,14 +59,18 @@ namespace Triangulator
 			return GetCWTypeForTriangle(randomTriangle);
 #endif
 
-#if USE_ROTARY_WALKING_METHOD
+#if USE_ROTARY_WALKING_METHOD || USE_PROPER_MATHS
 			var meshVertexLoopConverted = new List<Vector2>(meshVertexLoop.Count);
 			for (int i = 0; i < meshVertexLoop.Count; ++i)
 			{
 				meshVertexLoopConverted.Add(meshVertexLoop[i].ToVector2());
 			}
 
+#if USE_PROPER_MATHS
 			return GetCWTypeForVertexLoop(in meshVertexLoopConverted);
+#else
+			return GetCWTypeForVertexLoop(in meshVertexLoopConverted, evalMode);
+#endif
 #endif
 		}
 
@@ -78,46 +100,119 @@ namespace Triangulator
 #endif
 
 #if USE_ROTARY_WALKING_METHOD
-		private static CWType GetCWTypeForVertexLoop(in List<Vector2> vertexLoop)
+		private static CWType GetCWTypeForVertexLoop(in List<Vector2> vertexLoop, RotaryEvaluationPointMode evalMode)
 		{
 			// O(2n);
 			Debug.Assert(!vertexLoop.IsNullOrEmpty(), $"Can't walk the vertexloop as its null or empty");
 
-			Vector2 bariCenter = Vector2.zero;
-			float influence = 1.0f / vertexLoop.Count;
-
-			for (int i = 0; i < vertexLoop.Count; ++i)
-			{
-				bariCenter += (vertexLoop[i] * influence);
-			}
+			Vector2 evaluationPoint = GetEvaluationPoint(evalMode, in vertexLoop);
 
 			// So we take baricenter to vertex[0] as base.
 			// Then each vector between baricenter and vertex[i], we check against the previous vertex. We then add all the values together to see if we've got a ccw or cw 
 
 
 			float totalRotationAngles = 0f;
-			Vector2 previousVector = vertexLoop[0] - bariCenter;
+			Vector2 previousVector = vertexLoop[0] - evaluationPoint;
 
 			for (int i = 1; i < vertexLoop.Count + 1; ++i)
 			{
-				Vector2 deltaToPoint = vertexLoop[i % vertexLoop.Count] - bariCenter;
+				Vector2 deltaToPoint = vertexLoop[i % vertexLoop.Count] - evaluationPoint;
 				float newAngles = Vector2.SignedAngle(previousVector, deltaToPoint);
 				totalRotationAngles += newAngles;
 
 				previousVector = deltaToPoint;
 			}
 
-			if (totalRotationAngles.IsCloseTo(360))
+			if (totalRotationAngles.IsCloseTo(360, 0.005f))
 			{
 				return CWType.CCW;
 			}
-			else if (totalRotationAngles.IsCloseTo(-360))
+			else if (totalRotationAngles.IsCloseTo(-360, 0.005f))
 			{
+				return CWType.CW;
+			}
+
+			if (totalRotationAngles.IsCloseTo(0, 0.005f))
+			{
+				Debug.LogWarning($"The total rotation angles are close to 0, the way this occurs, is if the baricenter is outside of the shape, {totalRotationAngles}");
 				return CWType.CW;
 			}
 
 			Debug.LogError($"TotalRotationAngles are: {totalRotationAngles}, this is not a defined case.");
 			return CWType.CW;
+		}
+
+		public static Vector2 GetEvaluationPoint(RotaryEvaluationPointMode evalMode, in List<Vector2> vertexLoop)
+		{
+			var result = Vector2.zero;
+
+			Vector2 calculateBariCenter(List<Vector2> vertices)
+			{
+				Vector2 bariCenter = Vector2.zero;
+				float influence = 1.0f / vertices.Count;
+
+				for (int i = 0; i < vertices.Count; ++i)
+				{
+					bariCenter += (vertices[i] * influence);
+				}
+
+				return bariCenter;
+			}
+
+			switch (evalMode)
+			{
+				case RotaryEvaluationPointMode.Baricenter:
+				{
+					result = calculateBariCenter(vertexLoop);
+					break;
+				}
+
+				case RotaryEvaluationPointMode.RandomVertex:
+				{
+					var randomVertex = vertexLoop[0];
+					var secondVertex = vertexLoop[1];
+
+					var delta = secondVertex - randomVertex;
+
+					result = randomVertex + (delta.normalized * 0.1f);
+					break;
+				}
+
+				default:
+				{
+					Debug.LogError($"No case defined for evaluation mode: {evalMode}");
+					break;
+				}
+			}
+
+			return result;
+		}
+#endif
+
+#if USE_PROPER_MATHS
+		// This method uses the logic as described in: https://www.element84.com/blog/determining-the-winding-of-a-polygon-given-as-a-set-of-ordered-points
+		private static CWType GetCWTypeForVertexLoop(in List<Vector2> vertexLoop)
+		{
+			// sum_{n=1}^N(x_{n+1} – x_n)(y_{n+1} + y_n)
+			// If result is above 0, we are CW, if below 0, we are CCW
+			float totalSum = 0f;
+			for (int i = 0; i < vertexLoop.Count; ++i)
+			{
+				var currentVert = vertexLoop[i];
+				var nextVert = vertexLoop[(i + 1) % vertexLoop.Count];
+
+				var calcVector = new Vector2(nextVert.x - currentVert.x, nextVert.y + currentVert.y);
+				totalSum += calcVector.x * calcVector.y;
+			}
+
+			if (totalSum > 0)
+			{
+				return CWType.CW;
+			}
+			else
+			{
+				return CWType.CCW;
+			}
 		}
 #endif
 	}
