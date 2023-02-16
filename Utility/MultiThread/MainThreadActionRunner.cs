1527650cdbf6618953e0459c57337ea5df963e4f
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2023-02-17 00:05:34 +0100

    Added an object to run stuff on the main thread with, for when we're in multithreaded context

---
+++ Utility/MultiThread/MainThreadActionRunner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MainThreadActionRunner
+{
+	private const int MaxPerFrame = 10;
+
+	private ThreadChecker tc = null;
+	private Queue<System.Action> actions;
+
+	public MainThreadActionRunner()
+	{
+		tc = new ThreadChecker(); tc.SetMainThreadID();
+		actions = new Queue<System.Action>();
+	}
+
+	public void Update()
+	{
+		Debug.Assert(tc.IsCurrentThreadMainThread(), $"We are not on the main thread, please fix");
+
+		if ((actions?.Count ?? -1) != 0)
+		{
+			Debug.Log($"Update ThreadActionRunner, actionsCount: {actions?.Count ?? -1}");
+		}
+
+		for (int i = 0; i < MaxPerFrame; ++i)
+		{
+			System.Action nextAction = null;
+			if (actions.Count > 0)
+			{
+				nextAction = actions.Dequeue();
+			}
+
+			if (nextAction == null)
+			{
+				break;
+			}
+
+			nextAction.Invoke();
+		}
+	}
+
+	public void PushAction(System.Action action)
+	{
+		actions.Enqueue(action);
+	}
+}
