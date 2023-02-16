b2d8bacfa678a6f3d8b3d2d17046f113d543294a
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2023-02-17 00:05:45 +0100

    Added a simple object to test if we're on the main thread

---
+++ Utility/MultiThread/ThreadChecker.cs
@@ -0,0 +1,14 @@
+﻿public class ThreadChecker
+{
+	private int mainThreadID;
+
+	public void SetMainThreadID()
+	{
+		mainThreadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
+	}
+
+	public bool IsCurrentThreadMainThread()
+	{
+		return mainThreadID == System.Threading.Thread.CurrentThread.ManagedThreadId;
+	}
+}
