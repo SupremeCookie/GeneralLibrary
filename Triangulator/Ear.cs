6b1ab3e9725b4902586ca217ad4a765136602861
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2023-02-23 23:04:23 +0100

    Detecting line ears

--- Triangulator/Ear.cs
+++ Triangulator/Ear.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Triangulator
@@ -54,6 +55,45 @@ namespace Triangulator
 				area = EarUtility.CalculateArea(vertices);
 
 				bool hasArea = area > 0;
+				if (!hasArea)
+				{
+					// Note DK: Sometimes we can get ears that are actually lines, we can check this.
+					var sortedVertices = vertices.OrderBy(s => s.position.x).ToArray();
+					Vertex getLeftMostVertex()
+					{
+						return sortedVertices[0];
+					}
+
+					Vertex getRightMostVertex()
+					{
+						return sortedVertices[2];
+					}
+
+					Vertex getMiddleVertex()
+					{
+						return sortedVertices[1];
+					}
+
+
+					var farLeftVertex = getLeftMostVertex();
+					var farRightVertex = getRightMostVertex();
+					var middleVertex = getMiddleVertex();
+
+					var leftToRight = farRightVertex.position - farLeftVertex.position;
+					leftToRight.Normalize();
+
+					var leftToMiddle = middleVertex.position - farLeftVertex.position;
+					leftToMiddle.Normalize();
+
+					float dot = Vector2.Dot(leftToRight, leftToMiddle);
+					if (dot >= 0.98f)
+					{
+						// Note DK: This means the ear is a line.
+						Debug.LogWarning($"We've encountered an ear that's basically a line, this means we have 0 area");
+						return 0;
+					}
+				}
+
 				Debug.Assert(hasArea, $"Area is not above 0, this means CalculateArea failed, or was not correctly done, will set areaIsDirty to true so we can retry next time. Area: {area}");
 
 				areaIsDirty = false;
