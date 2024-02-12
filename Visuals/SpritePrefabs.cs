5d6b1b25013adf52b62f05bd7b10f0a93892f63f
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-02-13 00:18:53 +0100

    Added masked prefabs to the SpritePrefab

--- Visuals/SpritePrefabs.cs
+++ Visuals/SpritePrefabs.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class MaskedSpritePrefab
+{
+	public byte mask;
+	public GameObject spritePrefab;
+}
 
 public class SpritePrefabs : MonoBehaviour
 {
 	[SerializeField] private GameObject spritePrefab;
+	[SerializeField] private List<MaskedSpritePrefab> prefabs;
 
 	[SerializeField] private List<byte> masks = new List<byte>();
+	[SerializeField] private List<byte> missingMasks = new List<byte>();
 
 	public GameObject GetSpritePrefab(byte mask)
 	{
 		if (!masks.Contains(mask)) { masks.Add(mask); }
+
+		for (int i = 0; i < prefabs.Count; ++i)
+		{
+			if (prefabs[i].mask == mask)
+			{
+				return prefabs[i].spritePrefab;
+			}
+		}
+
+		if (!missingMasks.Contains(mask)) { missingMasks.Add(mask); }
 
 		return spritePrefab;
 	}
