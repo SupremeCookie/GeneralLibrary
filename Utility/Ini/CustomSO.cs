941cb90242f100ac19fd1f6fd29df2dc4220ca5f
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-03-18 12:20:47 +0100

    Moved some basic tooling from GeneralScriptableData to CustomSO

---
+++ Utility/Ini/CustomSO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CustomSO : ScriptableObject
+{
+	protected static CustomSO pInstance;
+
+	protected static T LoadScriptableObject<T>(string name) where T : CustomSO
+	{
+		var scriptableObject = Resources.Load(name) as T;
+		Debug.AssertFormat(scriptableObject != null, "ScriptableObject {0} is null", name);
+		return scriptableObject;
+	}
+
+#if UNITY_EDITOR
+	public static void OpenScriptableObject()
+	{
+		var scriptableObject = pInstance;
+		UnityEditor.Selection.activeObject = scriptableObject;
+#endif
+	}
+}
+
+#if UNITY_EDITOR
+[UnityEditor.CustomEditor(typeof(CustomSO))]
+public class CustomSODataEditor : UnityEditor.Editor
+{
+	public override void OnInspectorGUI()
+	{
+		var castedTarget = (CustomSO)target;
+
+		ScriptableUtility.DrawSaveButton_AtStart(target);
+
+		base.OnInspectorGUI();
+
+		UnityEditor.EditorGUILayout.Space();
+		UnityEditor.EditorGUILayout.Space();
+	}
+}
+#endif
