cf89618399a4e7d305580ab9a9d5ada4a6d982a6
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-03-31 22:34:32 +0200

    Added a 3d Draw Gizmo Collider

---
+++ Gizmos/GizmosDrawCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GizmosDrawCollider : MonoBehaviour
+{
+	[SerializeField] private Color gizmosColor = Color.white;
+
+#if UNITY_EDITOR
+	private Collider gizmoCollider;
+	private MeshCollider meshCollider;
+
+	private void Start() { }
+
+	private void OnDrawGizmos()
+	{
+		if (!this.enabled)
+		{
+			return;
+		}
+
+		if (gizmoCollider == null)
+		{
+			gizmoCollider = GetComponentInChildren<Collider>();
+			return;
+		}
+
+		if (gizmoCollider is MeshCollider)
+		{
+			if (meshCollider == null)
+			{
+				meshCollider = gizmoCollider as MeshCollider;
+			}
+			else
+			{
+				Gizmos.color = gizmosColor;
+				Gizmos.DrawMesh(meshCollider.sharedMesh);
+			}
+		}
+	}
+#endif
+}
