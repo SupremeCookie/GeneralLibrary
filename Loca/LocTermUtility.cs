d7cd69e3e6e67ccc8352b40844b66650da8c341b
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-07-23 15:26:14 +0200

    Renamed LocTerm to LocTermUtility, and moved  LocTermModel to its own folder

---
+++ Loca/LocTermUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LocTermUtility
+{
+	public static string GetLoca(string termKey, string fallback)
+	{
+		Debug.Assert(!string.IsNullOrEmpty(termKey), $"An empty key has been passed on, fallback value will be returned: {fallback}");
+
+		// TODO DK: Once loca is in, make something here.
+		fallback = fallback.Replace("\\n", System.Environment.NewLine);
+		return fallback;
+	}
+}
