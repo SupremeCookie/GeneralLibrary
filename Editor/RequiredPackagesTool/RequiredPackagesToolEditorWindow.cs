91725d0262d4c385fe92b284092a252ab908eac7
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-09-20 13:11:38 +0200

    Got the json manifest stuff to work

--- Editor/RequiredPackagesTool/RequiredPackagesToolEditorWindow.cs
+++ Editor/RequiredPackagesTool/RequiredPackagesToolEditorWindow.cs
@@ -33,13 +33,14 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 		string fullScriptFolderPath = Path.GetFullPath(scriptFolder);
 		string manifestContentPath = $"{fullScriptFolderPath}/manifestJson_newtonsoftPackageData.txt";
 		List<string> manifestJsonContent = new List<string>(File.ReadAllLines(manifestContentPath));
+
 		string[] manifestJsonDependencyValue = manifestJsonContent[0].Split(':');
 		string manifestJsonKey = manifestJsonDependencyValue[0];
 
-		for (int i = 0; i < manifestJsonContent.Count; ++i)
-		{
-			Debug.Log($"FileContent ManifestJson: {manifestJsonContent[i]}");
-		}
+		//for (int i = 0; i < manifestJsonContent.Count; ++i)
+		//{
+		//	Debug.Log($"FileContent ManifestJson: {manifestJsonContent[i]}");
+		//}
 
 		Debug.Log("----");
 
@@ -74,6 +75,12 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 					{
 						Debug.Log($"We found the dependency in the list, gotta set the value then to what we've got stored in the txt");
 						splitLine[1] = manifestJsonDependencyValue[1];
+
+						// If the entry is the last one in the dependencies list, we gotta remove the trailing ,
+						if (fileContent[i + 1].IndexOf("}") >= 0 && splitLine[1].EndsWith(","))
+							splitLine[1] = splitLine[1].Remove(splitLine[1].Length - 1);
+
+
 						packageDependencyWasPresent = true;
 					}
 
@@ -90,15 +97,75 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 			if (!fileContent[lastCheckedIndex].EndsWith(","))
 				fileContent[lastCheckedIndex] += ",";
 
-			fileContent.Insert(lastCheckedIndex + 1, manifestJsonContent[0]);
+			string contentToAdd = manifestJsonContent[0];
+			if (contentToAdd.EndsWith(","))
+				contentToAdd = contentToAdd.Remove(contentToAdd.Length - 1);
+
+			fileContent.Insert(lastCheckedIndex + 1, contentToAdd);
 		}
 
 		// scopedRegistries dependency
+		string scopedRegistriesName = manifestJsonContent[1].Split(":")[0];
+		string scopedRegistriesValueName = manifestJsonContent[3];
+		bool foundScopedRegistries = false;
+		bool foundOurEntry = false;
+
+		int lastCheckedIndexSR = 0;
+		int squareBracketStack = 0;
+
 		for (int i = 0; i < fileContent.Count; ++i)
 		{
+			if (fileContent[i].IndexOf(scopedRegistriesName) >= 0)
+			{
+				foundScopedRegistries = true;
+			}
 
+			if (foundScopedRegistries)
+			{
+				if (fileContent[i].IndexOf("[") >= 0)
+					squareBracketStack++;
+
+				if (fileContent[i].IndexOf("]") >= 0)
+					squareBracketStack--;
+
+				if (squareBracketStack > 0)
+					lastCheckedIndexSR = i;
+
+				if (fileContent[i].IndexOf(scopedRegistriesValueName) >= 0)
+				{
+					foundOurEntry = true;
+					for (int k = 3; k < (9 - 2); ++k)   // We start at the name part, and stop before the last squiggly bracket
+					{
+						fileContent[i + (k - 3)] = manifestJsonContent[k];
+					}
+				}
+			}
 		}
 
+		// Add the entire scopedRegistries part
+		if (!foundScopedRegistries)
+		{
+			int lastLineIndex = fileContent.Count - 1;
+			if (fileContent[lastLineIndex - 1].EndsWith("}"))
+				fileContent[lastLineIndex - 1] += ",";
+
+			fileContent.RemoveAt(lastLineIndex);
+			for (int i = 1; i < 10; ++i)
+			{
+				fileContent.Add(manifestJsonContent[i]);
+			}
+
+			fileContent.Add("}");
+		}
+		else if (!foundOurEntry)
+		{
+			// We add our registry stuff starting at the last checked indexSR
+			if (!fileContent[lastCheckedIndexSR].EndsWith(","))
+				fileContent[lastCheckedIndexSR] += ",";
+
+			for (int i = 2; i < 9; ++i)
+				fileContent.Insert(lastCheckedIndexSR + 1 + i - 2, manifestJsonContent[i]);
+		}
 
 		File.WriteAllLines(absoluteManifestPath, fileContent);
 	}
