5cb718f5fbd0e693bee639c0d050fe78c9d9eef4
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-09-20 11:24:45 +0200

    The first line of the manifest is now added or updated properly in the dependencies section

--- Editor/RequiredPackagesTool/RequiredPackagesToolEditorWindow.cs
+++ Editor/RequiredPackagesTool/RequiredPackagesToolEditorWindow.cs
@@ -3,6 +3,7 @@ using UnityEngine.UIElements;
 using UnityEditor;
 using System.IO;
 using System.Collections.Generic;
+using System;
 
 public class RequiredPackagesToolEditorWindow : EditorWindow
 {
@@ -12,10 +13,10 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 		RequiredPackagesToolEditorWindow window = GetWindow<RequiredPackagesToolEditorWindow>();
 		window.titleContent = new GUIContent($"{typeof(RequiredPackagesToolEditorWindow)}");
 	}
-	
+
 	public void OnGUI()
 	{
-		if(GUILayout.Button("Setup Newtonsoft"))
+		if (GUILayout.Button("Setup Newtonsoft"))
 		{
 			TrySetupNewtonsoft();
 		}
@@ -32,7 +33,10 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 		string fullScriptFolderPath = Path.GetFullPath(scriptFolder);
 		string manifestContentPath = $"{fullScriptFolderPath}/manifestJson_newtonsoftPackageData.txt";
 		List<string> manifestJsonContent = new List<string>(File.ReadAllLines(manifestContentPath));
-		for(int i = 0; i < manifestJsonContent.Count; ++i)
+		string[] manifestJsonDependencyValue = manifestJsonContent[0].Split(':');
+		string manifestJsonKey = manifestJsonDependencyValue[0];
+
+		for (int i = 0; i < manifestJsonContent.Count; ++i)
 		{
 			Debug.Log($"FileContent ManifestJson: {manifestJsonContent[i]}");
 		}
@@ -42,25 +46,60 @@ public class RequiredPackagesToolEditorWindow : EditorWindow
 		string absoluteManifestPath = Path.GetFullPath("Packages/manifest.json");
 		List<string> fileContent = new List<string>(File.ReadAllLines(absoluteManifestPath));
 
-		int lastCheckedIndex = -1;
-		bool dependencyWasPresent = false;
-		bool foundDependencies = true;
 
-		for(int i = 0; i < fileContent.Count; ++i)
+		// Dependency injection
+		int lastCheckedIndex = -1;
+		bool packageDependencyWasPresent = false;
+		bool foundDependencies = true;
+		for (int i = 0; i < fileContent.Count; ++i)
 		{
-			Debug.Log($"FileContent Line({i}): {fileContent[i]}");
-			if(fileContent[i].IndexOf("dependencies") >= 0)
+			//Debug.Log($"FileContent Line({i}): {fileContent[i]}");
+			if (fileContent[i].IndexOf("dependencies") >= 0)
 			{
 				foundDependencies = true;
-				string[] splitLine = fileContent[i].Split(':');
-				if(splitLine.Length > 1)
-				{
-					//if(splitLine[0].IndexOf(""))
-				}
 			}
 
-			if(foundDependencies)
-				lastCheckedIndex = i;
+			if (foundDependencies)
+			{
+				if (fileContent[i].IndexOf("}") >= 0 || fileContent[i].IndexOf("},") >= 0)
+				{
+					break;
+				}
+
+				string[] splitLine = fileContent[i].Split(':');
+				if (splitLine.Length > 1)
+				{
+					//Debug.Log($"Splitline 0: {splitLine[0]}   ,  key: {manifestJsonKey}");
+					if (splitLine[0].IndexOf(manifestJsonKey) >= 0) // Note: Equals don't work here, because there's leading spaces
+					{
+						Debug.Log($"We found the dependency in the list, gotta set the value then to what we've got stored in the txt");
+						splitLine[1] = manifestJsonDependencyValue[1];
+						packageDependencyWasPresent = true;
+					}
+
+					fileContent[i] = string.Join(':', splitLine);
+
+					lastCheckedIndex = i;
+				}
+			}
 		}
+
+		if (foundDependencies && !packageDependencyWasPresent)
+		{
+			Debug.Log($"The newtonsoft dependency will be added to the list");
+			if (!fileContent[lastCheckedIndex].EndsWith(","))
+				fileContent[lastCheckedIndex] += ",";
+
+			fileContent.Insert(lastCheckedIndex + 1, manifestJsonContent[0]);
+		}
+
+		// scopedRegistries dependency
+		for (int i = 0; i < fileContent.Count; ++i)
+		{
+
+		}
+
+
+		File.WriteAllLines(absoluteManifestPath, fileContent);
 	}
 }
