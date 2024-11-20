6a0fcbe8d1162e3ee82a0a3b0d967a3be18bd23a
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2024-11-20 10:08:31 +0100

    Added a conditional load structure to the BootStrapper, this way imgui is only loaded if we desire it

--- Utility/Scenes/BootStrapper.cs
+++ Utility/Scenes/BootStrapper.cs
@@ -2,12 +2,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[System.Serializable]
+public class ConditionalLoad
+{
+	public enum Condition
+	{
+		None,
+		Imgui,
+	}
+
+	public Condition condition;
+	public string[] scenes;
+}
+
 public class BootStrapper : MonoBehaviour
 {
 	// TODO DK: Make editor script that takes SceneAsset type, then a button to turn that list into a list of readonly strings that then get actually loaded.
 	// That way I avoid typos.
 
 	public string[] ScenesToLoad;
+	public ConditionalLoad[] conditionalLoads;
 
 	public void Awake()
 	{
@@ -17,10 +31,53 @@ public class BootStrapper : MonoBehaviour
 			openScenes.Add(SceneManager.GetSceneAt(i).name);
 		}
 
-		for (int i = 0; i < ScenesToLoad.Length; ++i)
+
+		if (ScenesToLoad.Length > 0)
 		{
-			if (!openScenes.Contains(ScenesToLoad[i]))
-				SceneManager.LoadScene(ScenesToLoad[i], LoadSceneMode.Additive);
+			for (int i = 0; i < ScenesToLoad.Length; ++i)
+			{
+				if (!openScenes.Contains(ScenesToLoad[i]))
+					SceneManager.LoadScene(ScenesToLoad[i], LoadSceneMode.Additive);
+			}
+		}
+
+		if (conditionalLoads.Length > 0)
+		{
+			for (int i = 0; i < conditionalLoads.Length; ++i)
+			{
+				for (int k = 0; k < conditionalLoads[i].scenes.Length; ++k)
+				{
+					if (!HasConditionBeenMet(conditionalLoads[i].condition))
+						continue;
+
+					if (!openScenes.Contains(conditionalLoads[i].scenes[k]))
+						SceneManager.LoadScene(conditionalLoads[i].scenes[k], LoadSceneMode.Additive);
+				}
+			}
+		}
+	}
+
+	private bool HasConditionBeenMet(ConditionalLoad.Condition condition)
+	{
+		switch (condition)
+		{
+			case ConditionalLoad.Condition.None:
+				return true;
+
+			case ConditionalLoad.Condition.Imgui:
+			{
+#if DEBUG_MENU
+				return true;
+#else
+				return false;
+#endif
+			}
+
+			default:
+			{
+				Debug.Log($"No condition logic made for: {condition}, will be returning true");
+				return true;
+			}
 		}
 	}
 }
