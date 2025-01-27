25d7dc2fdaa9ef5ecf06f4487e9dfaba26546ecd
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-01-27 05:58:45 +0100

    Improved GizmosDrawRect slightly

--- Gizmos/GizmosDrawRect.cs
+++ Gizmos/GizmosDrawRect.cs
@@ -4,12 +4,14 @@ public class GizmosDrawRect : MonoBehaviour
 {
 	public Vector3 gizmosSize;
 	public Color gizmosColor = Color.white;
+	[Space(5)]
+	public Vector3 offset;
 
 #if UNITY_EDITOR
 	private void OnDrawGizmos()
 	{
 		Gizmos.color = gizmosColor;
-		Gizmos.DrawCube(transform.position, gizmosSize);
+		Gizmos.DrawCube(transform.position + offset, gizmosSize);
 	}
 #endif
 }
