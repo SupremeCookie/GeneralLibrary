b8a5a1a28d087f353273f77f798830e6d0f5eae3
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-01-28 19:20:55 +0100

    Added a PersistentVector2

---
+++ ScriptableObjects/PersistentVector2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = VECTOR2_SO_NAME, menuName = "_scriptables/Utility/" + VECTOR2_SO_NAME, order = 0)]
+public class PersistentVector2 : ScriptableObject
+{
+	public const string VECTOR2_SO_NAME = "Persistent-Vector2";
+
+	[SerializeField] private Vector2 value;
+
+	public Vector2 GetValue() => value;
+}
