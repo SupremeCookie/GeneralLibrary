77382d1682a9bc463604f68b315f51fa980451f6
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-01-28 09:59:32 +0100

    Added a PersistentFloat scriptable object to allow for persistent single values

---
+++ ScriptableObjects/PersistentFloat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = FLOAT_SO_NAME, menuName = "_scriptables/Utility/" + FLOAT_SO_NAME, order = 0)]
+public class PersistentFloat : ScriptableObject
+{
+	public const string FLOAT_SO_NAME = "Persistent-Float";
+
+	[SerializeField] private float value;
+
+	public float GetValue() => value;
+}
