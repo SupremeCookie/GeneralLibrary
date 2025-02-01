785de945b20ab2ac4426a1090d9498c70d975e02
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-02-01 19:58:05 +0100

    Added a scalable persistent vector2

---
+++ ScriptableObjects/PersistentScalableVector2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = SCALABLE_VECTOR2_SO_NAME, menuName = "_scriptables/Utility/" + SCALABLE_VECTOR2_SO_NAME, order = 0)]
+public class PersistentScalableVector2 : ScriptableObject
+{
+	public const string SCALABLE_VECTOR2_SO_NAME = "Persistent-Scalable-Vector2";
+
+	[SerializeField] private Vector2 value;
+	[SerializeField] private float scale = 1.0f;
+
+	public Vector2 GetValue() => value * scale;
+}
