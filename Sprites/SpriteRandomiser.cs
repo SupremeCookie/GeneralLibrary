1d45f9143a423ef29d165079b8378a1175692e81
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-02-13 23:47:22 +0100

    Improved sprite randomiser

--- Sprites/SpriteRandomiser.cs
+++ Sprites/SpriteRandomiser.cs
@@ -7,6 +7,11 @@ public class SpriteRandomiser : MonoBehaviour
 	[SerializeField] private SpriteRenderer spriteRenderer;
 	[Space(10)]
 	[SerializeField] private List<Sprite> randomSprites;
+	[Header("Settings")]
+	[SerializeField] private bool randomiseManually = false;
+
+	private int _randomSpriteIndex = -1;
+	public int randomSpriteIndex => _randomSpriteIndex;
 
 	private void Reset()
 	{
@@ -15,10 +20,11 @@ public class SpriteRandomiser : MonoBehaviour
 
 	private void Start()
 	{
-		RandomiseSprite();
+		if (!randomiseManually)
+			RandomiseSprite();
 	}
 
-	private void RandomiseSprite()
+	public void RandomiseSprite()
 	{
 		if (randomSprites.IsNullOrEmpty())
 		{
@@ -26,6 +32,7 @@ public class SpriteRandomiser : MonoBehaviour
 			return;
 		}
 
-		spriteRenderer.sprite = randomSprites[Random.Range(0, randomSprites.Count)];
+		_randomSpriteIndex = Random.Range(0, randomSprites.Count);
+		spriteRenderer.sprite = randomSprites[_randomSpriteIndex];
 	}
 }
