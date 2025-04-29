c32d477a812e6f52d22171d9cc9b74b2b57e2c85
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-04-29 14:18:02 +0200

    Added a PersistentVector3

---
+++ ScriptableObjects/PersistentVector3.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = VECTOR3_SO_NAME, menuName = "_scriptables/Utility/" + VECTOR3_SO_NAME, order = 0)]
+public class PersistentVector3 : ScriptableObject
+{
+	public const string VECTOR3_SO_NAME = "Persistent-Vector3";
+
+	[SerializeField] private Vector3 value;
+
+	public Vector3 GetValue() => value;
+}
