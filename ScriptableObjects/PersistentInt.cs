1c6c15dc39d9220987cacca3d396df3ce936d7c1
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-07-20 06:45:16 +0200

    Added a persistent int

---
+++ ScriptableObjects/PersistentInt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = INT_SO_NAME, menuName = "_scriptables/Utility/" + INT_SO_NAME, order = 0)]
+public class PersistentInt : ScriptableObject
+{
+	public const string INT_SO_NAME = "Persistent-Int";
+
+	[SerializeField] private int value;
+
+	public int GetValue() => value;
+}
