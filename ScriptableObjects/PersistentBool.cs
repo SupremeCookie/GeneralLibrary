6f0fa4359ccf5308d6b09c21c92538631f8a6125
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2025-12-30 22:27:14 +0100

    Added a persistent bool

---
+++ ScriptableObjects/PersistentBool.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = BOOL_SO_NAME, menuName = "_scriptables/Utility/" + BOOL_SO_NAME, order = 0)]
+public class PersistentBool : ScriptableObject
+{
+	public const string BOOL_SO_NAME = "Persistent-Bool";
+
+	[SerializeField] private bool value;
+
+	public bool GetValue() => value;
+	public bool SetValue(bool value) => this.value = value;
+}
