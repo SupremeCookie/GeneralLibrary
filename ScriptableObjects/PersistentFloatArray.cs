0a961fc442fd608b249aac4d7d3ff550edf470b2
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2026-01-19 07:52:13 +0100

    Added a persistent float array

---
+++ ScriptableObjects/PersistentFloatArray.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+[CreateAssetMenu(fileName = FLOAT_SO_NAME, menuName = "_scriptables/Utility/" + FLOAT_SO_NAME, order = 100)]
+public class PersistentFloatArray : ScriptableObject
+{
+	public const string FLOAT_SO_NAME = "Persistent-FloatArray";
+
+	[SerializeField] private float[] value;
+
+	public float[] GetValue() => value;
+}
