bfc1dcae95cadfa46433cce737eb78767d612711
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2026-06-20 15:51:39 +0200

    Improved logging in LocTermUtility

--- Loca/LocTermUtility.cs
+++ Loca/LocTermUtility.cs
@@ -8,7 +8,7 @@ public class LocTermUtility
 		LocaReportingTool.Instance.RegisterLocterm(new LocTermModel(termKey, fallback));
 #endif
 
-		Debug.Assert(!string.IsNullOrEmpty(termKey), $"An empty key has been passed on, fallback value will be returned: {fallback}");
+		Debug.Assert(!string.IsNullOrEmpty(termKey), $"An empty key has been passed on, fallback value will be returned: {(string.IsNullOrEmpty(fallback) ? "empty-string" : fallback)}");
 
 		// TODO DK: Once loca is in, make something here.
 		fallback = fallback.Replace("\\n", System.Environment.NewLine);
