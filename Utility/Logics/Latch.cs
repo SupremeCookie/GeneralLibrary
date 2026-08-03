fe1c12e8fc163bd88a89f2b88c3c57b46ff49025
David Katsma (SupremeCookie) <davidlskatsma@gmail.com>
2026-08-03 22:47:22 +0200

    Added a Latch model

---
+++ Utility/Logics/Latch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class Latch
+{
+	public float latchValue;
+	public bool hasJustLatched = false;
+	public float currentLatchValue = 0f;
+
+	private bool hasFinished = false;
+
+	public Latch() { }
+	public Latch(float latchValue)
+	{
+		this.latchValue = latchValue;
+	}
+
+	public Latch Copy()
+	{
+		return new Latch()
+		{
+			latchValue = this.latchValue,
+			hasJustLatched = this.hasJustLatched,
+			currentLatchValue = this.currentLatchValue,
+			hasFinished = this.hasFinished,
+		};
+	}
+
+	public void Update(float dt)
+	{
+		currentLatchValue += dt;
+
+		if (hasJustLatched)
+			hasJustLatched = false;
+
+		if (hasFinished)
+			return;
+
+		if (currentLatchValue > latchValue)
+		{
+			hasJustLatched = true;
+			hasFinished = true;
+		}
+	}
+}
